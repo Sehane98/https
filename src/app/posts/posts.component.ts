3dbafed3fc0041ae63ef3e6f6fe608960ae881ad
Gulshen Mahmudova <g.mahmudova1@gmail.com>
2020-01-28 16:37:51 +0400

    final

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../services/post.service'
 
 
 @Component({
@@ -6,45 +7,46 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   
   posts: [any];
-  constructor() { 
 
-  	http.get("https://jsonplaceholder.typicode.com/posts")
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+  	this.postService.getPosts()
   		.subscribe(response=> {
   			this.posts = <[any]>response;
   		})
-  }
+  }  
 
   createPost(input: HTMLInputElement){
   	const post = { title: input.value }
 
-  	this.http.post(this.url,JSON.stringify(post))
-  		.subscribe(response=> {
+    this.postService.createPost(JSON.stringify(post))
+   		.subscribe(response=> {
   			input.value = '';
   			post['id'] = response['id'];
   			this.posts.splice(0, 0, post)
-  			console.log(response);
-  		})
+   		})
   }
 
   updatePost(post){
   	post.title = 'updated';
-  	// this.http.put(this.url + '/' + post.id, JSON.stringify(post))
-  	// 	.subscribe(response => {
-  	// 		console.log(response);
-  	// 	})
+    this.postService.updatePost(post)
+   		.subscribe(response => {
+  			console.log(response);
+  		})
 
-  	this.http.patch(this.url + '/' + post.id, JSON.stringify({title: 'updated'}))
-	  	.subscribe(response => {
-	  		console.log(response);
-	  	})
+  	// this.http.patch(this.url + '/' + post.id, JSON.stringify({title: 'updated'}))
+	  // 	.subscribe(response => {
+	  // 		console.log(response);
+	  // 	})
   }
 
   deletePost(post){
-  	this.http.delete(this.url + '/' + post.id)
-		.subscribe(response => {
+    this.postService.deletePost(post) 
+ 		.subscribe(response => {
  	  		let index = this.posts.indexOf(post);
 	  		this.posts.splice(index,1)
 	  	})
